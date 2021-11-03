92f292d7ac544021e4fac723fce83795214479c3
RominM <rominmanogil.off@gmail.com>
2021-11-03 16:27:26 +0100

    write the rules and style them

--- js/rules.js
+++ js/rules.js
@@ -14,26 +14,96 @@ export const rules = () => {
    const h2 = document.createElement('h2');
    h2.innerHTML = 'Game rules';
 
-   const p = document.createElement('p');
-   p.innerHTML = 'The player moves the racket from right to left to prevent the ball from falling into the area below. Each time the ball hits a brick, it disappears and the score increases by 1 point. The game ends if the ball falls below the racket or if all the bricks have been broken.'
-   //The ball and racket speeds are 5 at the start and increase by 0.25 each time the ball hits the racket.//
-   'The score is displayed when the game is over.'
+   const p1 = document.createElement('p');
+   p1.innerHTML = 
+   "Hi there! Move the paddle from left to right with keyboard to prevent the ball from falling below it. Be careful to keep the ball in the zone or you will lose a life ! br>\
+   Break all the bricks and make a maximum of point to win and pass a higher level."
+
+   const p2 = document.createElement('p');
+   p2.innerHTML = "Point by brick:<br>\
+   - White = 4pts<br>\
+   - Green = 8pts<br>\
+   - Blue = 12pts<br>\
+   - Red = 20pts"
+
+   const p3 = document.createElement('p');
+   p3.innerHTML = "The game is over when there are no more bricks or the ball has died under the racket and there is no more life to save you.<br>\
+   <br>\
+   The ball and the paddle accelerate each time a brick is hit."
+
+   const p4 = document.createElement('p');
+   p4.innerHTML = "You'll find picking up bonuses when these appear.<br>\
+   BONUS:"
+
+   const p5 = document.createElement('p');
+   p5.classList.add('p-bonus');
+   p5.innerHTML = "- Point multiplier (x1.5 for each brick).<br>\
+   - Point multiplier (x2 for each brick).<br>\
+   - Point multiplier (x2.5 for each brick).<br>\
+   - Speed ​​reducer: Restart the incrementation of zero(for 15 seconds). <br>\
+   - Laser cannon: Shoot the bricks with a laser(for 15 seconds).<br>\
+   - Glue pot: Glue the ball to the paddle, click to relaunch(for 20 seconds).<br>\
+   - SuperBall: The ball ignites and crosses all the bricks in its path(for 15 seconds).<br>\
+   - BONUS 50 Points<br>\
+   - BONUS 100 Points<br>\
+   - BONUS 200 Points<br>\
+   - ADDITIONAL LIFE BONUS"
+
+   const p6 = document.createElement('p');
+   p6.innerHTML = "Watch out for the Malus !!!<br>\
+   PENALTY:"
+
+   const p7 = document.createElement('p');
+   p7.classList.add('p-penalty')
+   p7.innerHTML = "- Braking out: Significantly reduces the movement of the paddle(for 20 seconds)."
+   
+   const p8 = document.createElement('p');
+   p8.innerHTML = "Bonuses and Maluses can appear on any brick and randomly.<br>\
+   <br>\
+   Save the ball and BREAK EVERYTHING!";
 
    closeRulesBtn.append(crossImg);
    bgRules.append(closeRulesBtn);
    bgRules.append(h2);
-   bgRules.append(p);
+   bgRules.append(p1, p2, p3, p4, p5, p6, p7, p8);
 
    body.append(bgRules);
 }
 
-// Les règles du jeu :
-//  Le joueur déplace la raquette de droite à gauche pour empêcher la balle de tomber dans
-// la zone en dessous
-//  A chaque fois que la balle touche une brique, elle disparaît et le score augmente de 1
-// point.
-//  Le jeu s’arrête si la balle tombe en dessous de la raquette ou si toutes les briques ont été
-// cassées.
-//  Les vitesses de la balle et de la raquette sont de 5 au départ et augmentent de 0.25 à
-// chaque fois que la balle touche la raquette.
-//  Le score s’affiche lorsque le jeu est fini.
+/*
+Hi there! Move the paddle from left to right with keyboard to prevent the ball from falling below it.
+Be careful to keep the ball in the zone or you will lose a life !
+Break all the bricks and make a maximum of point to win and pass a higher level.
+
+Point by brick:
+- White = 4pts
+- Green = 8pts
+- Blue = 12pts
+- Red = 20pts
+
+The game is over when there are no more bricks or the ball has died under the racket and there is no more life to save you.
+
+The ball and the racket accelerate each time a brick is hit.
+
+You'll find picking up bonuses when these appear.
+BONUS:
+- Point multiplier (x1.5 for each brick).
+- Point multiplier (x2 for each brick).
+- Point multiplier (x2.5 for each brick).
+- Speed ​​reducer: Restart the incrementation of zero. // for 15 seconds
+- Laser cannon: Shoot the bricks with a laser. // for 15 seconds
+- Glue pot: Glue the ball to the paddle, click to relaunch. // for 20 seconds
+- SuperBall: The ball ignites and crosses all the bricks in its path // 15 seconds
+- BONUS 50 Points
+- BONUS 100 Points
+- BONUS 200 Points
+- ADDITIONAL LIFE BONUS
+
+Watch out for the Malus !!!
+PENALTY:
+- Braking out: Significantly reduces the movement of the paddle. // 20 seconds
+
+Bonuses and Maluses can appear on any brick and randomly.
+
+Save the ball and BREAK EVERYTHING!
+*/
